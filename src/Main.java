cf29176cbcdcd9c62d8c864c626ea5eabb39d713
PaulSPuntusov <pps_ru@mail.ru>
2024-08-08 23:05:30 +0300

    Polimorph Task1 Point3D

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Point3D p3d = new Point3D(1,2,3);
+        System.out.println(p3d.toString());
     }
 }
