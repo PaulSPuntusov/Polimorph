e3fe7cd30d94337b2e7ca269f920f944f45084c9
PaulSPuntusov <pps_ru@mail.ru>
2024-08-09 22:36:20 +0300

    Polimorph Task6 Summation

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        Fraction f = new Fraction(5,6);
-        System.out.println(f.toString());
-        System.out.println(f.intValue());
-        System.out.println(f.longValue());
-        System.out.println(f.floatValue());
-        System.out.println(f.doubleValue());
-
+        System.out.println(sum(2,new Fraction(3,5),2.3));
+        System.out.println(sum(3.6,new Fraction(49,12),3,new Fraction(3,2)));
+        System.out.println(sum(new Fraction(1,3),1));
     }
-    public static double mainGetLength(LengthAble object){
+
+    public static double sum(Number... nums) {
+        double res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            res += nums[i].doubleValue();
+        }
+        return res;
+    }
+
+    public static double mainGetLength(LengthAble object) {
         double res = 0;
         res = object.getLength();
         return res;
     }
+
     interface LengthAble {
         double getLength();
     }
