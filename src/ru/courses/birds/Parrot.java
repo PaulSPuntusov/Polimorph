d67f834eaf6d6ca53499d7904b4a88898dfe8c2c
PaulSPuntusov <pps_ru@mail.ru>
2024-08-17 19:39:20 +0300

    Polimorph Task12 - добавил бажог в попугае нашел и исправил

--- src/ru/courses/birds/Parrot.java
+++ src/ru/courses/birds/Parrot.java
@@ -10,7 +10,7 @@ public class Parrot extends Birds{
 
     @Override
     public void sing() {
-        int textLength = 1+ (int)(Math.random()*(toString().length()));
+        int textLength = 1+ (int)((Math.random()*(text.length()))-1);
         for( int i = 0; i<textLength;i++){
             System.out.print(text.charAt(i));
         }
